7b6f3d8749e4c22bf57f185a1df69c337c6d5231
migue <211218@ids.upchiapas.edu.mx>
2024-06-03 23:01:38 -0600

    fisrt comit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.get('/hello', (req, res) => {
+    res.send("Hello World")
+});
+
+app.get('/jenkins', (req, res) => {
+    res.send("Hello Jenkins");
+});
+
+app.get('/bye', (req, res) => {
+    res.send("Bye World");
+});
+
+app.listen(port, () => {
+    console.log(`SERVER RUNNING IN http://localhost:${port}.`);
+});
